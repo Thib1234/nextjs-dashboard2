f80d5e0d0c88be106f9bfa471a06d53fce3ad608
Thib <vandenbemden.2110.student@ifosupwavre.be>
2024-07-14 17:24:22 +0200

    updated invoices/searching

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -4,13 +4,15 @@ import {
   CheckIcon,
   ClockIcon,
   CurrencyDollarIcon,
+  CurrencyEuroIcon,
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
+import { createInvoice } from '@/app/lib/actions';
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
   return (
-    <form>
+    <form action={createInvoice}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -56,6 +58,26 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
             </div>
           </div>
         </div>
+        {/* Invoice Reprise */}
+
+        {/* <div className="mb-4">
+          <label htmlFor="reprise" className="mb-2 block text-sm font-medium">
+            Montant Reprises
+          </label>
+          <div className="relative mt-2 rounded-md">
+            <div className="relative">
+              <input
+                id="reprise"
+                name="reprise"
+                type="number"
+                step="0.01"
+                placeholder="Enter USD reprise"
+                className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+              />
+              <CurrencyEuroIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+            </div>
+          </div>
+        </div> */}
 
         {/* Invoice Status */}
         <fieldset>
