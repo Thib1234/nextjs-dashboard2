bb49c194ef71faf466cb20f28c3b69481d56a905
Thib <vandenbemden.2110.student@ifosupwavre.be>
2024-07-17 15:58:09 +0200

    trying stuff

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -17,7 +17,9 @@ export default function LoginForm() {
     undefined,
   );
   return (
+    
     <form action={formAction} className="space-y-3">
+      <h1>IZI</h1>
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
         <h1 className={`${lusitana.className} mb-3 text-2xl`}>
           Please log in to continue.
